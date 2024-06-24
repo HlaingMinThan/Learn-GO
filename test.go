8dd75dc05be82fe7e8ed4f59ad185230bf7620af
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-06-24 15:47:30 +0700

    formatted string

--- test.go
+++ test.go
@@ -5,10 +5,25 @@ import "fmt" //formatting
 // go will automatically fire this "main" function
 // How to run - go run filename
 func main() {
-	// number := 13.4
-	// number -= 14
-	// fmt.Println(number)
+	// age := 23
+	// name := "hlaing min than"
+	number := 23.55
 
-	var number int8 = 127
-	fmt.Println(number)
+	//Printf (print with formatted string)
+	// %v
+	// fmt.Printf("My name is %v.my age is %v", name, age)
+
+	// %q
+	// fmt.Printf("My name is %q.my age is %q", name, age) //quote is only for string
+
+	// %T
+	// fmt.Printf("name is a type of %T", name)
+
+	// %f
+	// %0.1f
+	// fmt.Printf("age is %0.1f", number)
+
+	//Sprintf (save print with formatted string)
+	var saved = fmt.Sprintf("age is %0.1f", number)
+	fmt.Println(saved)
 }
