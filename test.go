1578a1eea32ec466fc022a6a19f861d3d518e172
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-06-24 15:09:35 +0700

    go variable

--- test.go
+++ test.go
@@ -5,5 +5,13 @@ import "fmt" //formatting
 // go will automatically fire this "main" function
 // How to run - go run filename
 func main() {
-	fmt.Println("hello world")
+	// var nameOne string = "hlaing min than"
+	// var nameTwo = "hlaing min than 2"
+	// var nameThree string
+
+	//shorthand for (var nameFour string = "name four")
+	//can't use outside of the function
+	nameFour := "name four"
+	nameFour = "name four 23"
+	fmt.Println(nameFour)
 }
