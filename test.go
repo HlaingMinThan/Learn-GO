f8baab69a7a094c78bbe07d629455bb64945f840
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-06-24 16:48:47 +0700

    string package

--- test.go
+++ test.go
@@ -1,27 +1,15 @@
 package main
 
-import "fmt"
-
-//formatting
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	//array
-	// var ages [3]int = [3]int{10, 20}
-
-	// var ages = [3]int{1, 2, 3}
-	// ages[1] = 100
-	// ages := [3]int{1, 2, 3}
-
-	// fmt.Println(ages)
-	// fmt.Println(len(ages)) //get array length
-
-	//slices push -> apppend can only used on slices
-	slices := []int{1, 2, 3, 4}
-	slices = append(slices, 30)
-
-	// fmt.Println(slices)
-	// fmt.Println(len(slices))
-	// fmt.Println(slices[1:3]) // ranges[index:index] -> ans : [2,3]
-	fmt.Println(slices[:3]) // ranges[index:index] -> ans : [1,2,3]
-	fmt.Println(slices[1:]) // ranges[index:index] -> ans : [2,3,4,30]
+	welcome := "greeting"
+	// fmt.Println(strings.Contains(welcome, "g"))
+	fmt.Println(strings.ReplaceAll(welcome, "e", "a")) //graating
+	fmt.Println(strings.Index(welcome, "ting"))        //4
+	fmt.Println(strings.ToUpper(welcome))              //GREETING
+	fmt.Println(strings.Split(welcome, "t"))           //GREETING
 }
