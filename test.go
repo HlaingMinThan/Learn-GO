9228649623a6f12ad732f8b38a22481c42dfe2a4
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-06-25 20:05:31 +0700

    map

--- test.go
+++ test.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	sayGreeting("hmt")
-	fmt.Println(names)
+	person := map[string]string{
+		"name": "hlaingminthan",
+		"age":  "20",
+	}
+
+	fmt.Println(person)
+	person["name"] = "updated"
+	person["contact"] = "contact"
+	person["age"] = "updated 1w"
+	fmt.Println(person["name"])
+	fmt.Println(person["age"])
+	fmt.Println(person)
 }
