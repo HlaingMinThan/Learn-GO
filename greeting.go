e4e8f5a19837c9b3be49b6d1f0aa418c4d073fd8
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-06-25 19:43:49 +0700

    package scope

---
+++ greeting.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var names = []string{"Hlaing", "Min", "Zaw"}
+
+func sayGreeting(n string) {
+	fmt.Println("Hello, " + n)
+}
--- test.go
+++ test.go
@@ -1,30 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func initials(name string) (string, string) {
-	initials := []string{}
-	splittedNames := strings.Split(name, " ")
-	for _, name := range splittedNames {
-		upperCase := strings.ToUpper(name)
-		initials = append(initials, upperCase[:1])
-	}
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-	return initials[0], "_"
-}
+import "fmt"
 
 func main() {
-	name1 := "Hlaing Min"
-	name2 := "Zaw"
-
-	s1, s2 := initials(name1)
-	fmt.Println(s1, s2)
-
-	s3, s4 := initials(name2)
-	fmt.Println(s3, s4)
+	sayGreeting("hmt")
+	fmt.Println(names)
 }
