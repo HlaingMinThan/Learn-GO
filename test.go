019d2a0b1877a78138184f557c87c8e205549134
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-06-27 19:45:34 +0700

    add pointers

--- test.go
+++ test.go
@@ -4,24 +4,12 @@ import (
 	"fmt"
 )
 
-func testPassByReference(m map[string]string) {
-	m["age"] = "override"
-}
-func testPassByValue(n string) {
-	n = "override"
+func testPassByValue(n *string) {
+	*n = "override"
 }
 
 func main() {
-	//pass by reference -> map,slices,functions
-	person := map[string]string{
-		"name": "hlaingminthan",
-		"age":  "20",
-	}
-	testPassByReference(person)
-	fmt.Println(person) //override the original person map data
-
-	//pass by value -> int,floats,strings,struct,array,bools
 	name := "Hlaingminthan"
-	testPassByValue(name)
+	testPassByValue(&name)
 	fmt.Println(name) //not override the original name
 }
